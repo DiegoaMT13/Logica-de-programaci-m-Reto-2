c21f537928c180406205627a9738bf7a5f15f9ea
Diego Armando Martinez Tapias <damt13a@gmail.com>
2024-06-23 20:25:44 -0500

    lógica de programación Reto 2

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+//Pregunta
+let respuesta = prompt("¿Qué día de la semana es?");
+//filtro 
+
+
+if (respuesta === "Sábado" || respuesta === "Domingo") {
+  alert("¡Buen fin de semana!");
+} else {
+  alert("¡Buena semana!");
+}
