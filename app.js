e0dcba141b07d98f2e15006ae0d8bc6ab72d4b9d
Diego Armando Martinez Tapias <damt13a@gmail.com>
2024-06-26 10:15:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+//Este punto lo copié del tutor no conocía el if con 2 argumentos pero aprendí con este caso y los 3 === MIL DISCULPAS 
 //Pregunta
 let respuesta = prompt("¿Qué día de la semana es?");
 //filtro 
